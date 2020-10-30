89f97a1ef8f866e4de5a0160083fe0194413388f
Joao Pedro <802.018@alunos.unigran.br>
2020-10-30 10:47:14 -0300

    base react

---
+++ react3/src/component/MeuComponente.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+
+
+class MeuComponente extends Component{
+
+    constructor(props){
+        super(props);
+        
+        //this.state = {numero: 5}
+    
+       // this.setState({numero:5})
+    
+       // this.state.numero =5
+    }
+
+    render(){
+        return <div>
+            <p>Ola eu sou em respeitador de casadas</p>
+        </div>
+    }
+
+
+}
+
+export default MeuComponente;
