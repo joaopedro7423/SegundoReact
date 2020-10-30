d6d555b906569f6f93698551d908b253b313215e
Joao Pedro <802.018@alunos.unigran.br>
2020-10-30 14:30:47 -0300

    Rotas e Hooks

--- react3/src/App.js
+++ react3/src/App.js
@@ -1,16 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, NavLink, Switch, Route } from 'react-router-dom';
 
-import MeuComponente from './component/MeuComponente';
-
-
-
+import Usuarios from './components/Usuarios/Usuarios'
+import AdicionarUsuario from './components/AdicionarUsuario/AdicionarUsuario'
+import DetalhesUsuario from './components/DetalhesUsuario/DetalhesUsuario'
+import Home from './components/Home/Home'
 
 function App() {
   return (
-    <div className="App">
-      <MeuComponente titulo ="Olá mundo"/>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/" exact>Início</NavLink>
+              </li>
+              <li>
+                <NavLink to="/usuarios">Usuários Cadastrados</NavLink>
+              </li>
+              <li>
+                <NavLink to="/adicionar">Adicionar Usuário</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/usuarios/:id">
+              <DetalhesUsuario />
+            </Route>
+            <Route path="/usuarios">
+              <Usuarios />
+            </Route>
+            <Route path="/adicionar">
+              <AdicionarUsuario />
+            </Route>
+            <Route path="*">
+              <h1>404</h1>
+              <p>Página não encontrada!</p>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
