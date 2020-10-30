378a5ab4cf5cb9a2c98850d8503e55de8f4ec787
Joao Pedro <802.018@alunos.unigran.br>
2020-10-30 10:46:58 -0300

    react base

---
+++ react3/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import MeuComponente from './component/MeuComponente';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <MeuComponente/>
+    </div>
+  );
+}
+
+export default App;
